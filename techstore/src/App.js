b7a004dfb73280a3d893e3207579f535f6106bdd
gustavo caris <149839159+Gustavocaris@users.noreply.github.com>
2024-06-22 15:01:01 -0300

    pacotes

--- techstore/src/App.js
+++ techstore/src/App.js
@@ -1,8 +1,32 @@
+import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+   <Router>
+     <div className="App">
+      <div className="nav"> 
+        <h1 className="logo">Tech <span>Store</span></h1>
+        <nav>
+          <ul>
+            <li>
+              <link to="/">Home</link>
+            </li>
+            <li>
+              <link to="/">Produtos</link>
+            </li>
+            <li>
+              <link to="/">Sobre</link>
+            </li>
+            <li>
+              <link to="/">Contatos</link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    
     </div>
+   </Router>
   );
 }
 
